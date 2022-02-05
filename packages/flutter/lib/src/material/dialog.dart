402fd1321c2c5fdc807fa84e7222523e27c84ae1
Callum Moffat <smartercallum@gmail.com>
2022-02-05 01:01:55 -0500

    MediaQuery as InheritedModel

--- packages/flutter/lib/src/material/dialog.dart
+++ packages/flutter/lib/src/material/dialog.dart
@@ -538,7 +538,7 @@ class AlertDialog extends StatelessWidget {
 
     // The paddingScaleFactor is used to adjust the padding of Dialog's
     // children.
-    final double paddingScaleFactor = _paddingScaleFactor(MediaQuery.of(context).textScaleFactor);
+    final double paddingScaleFactor = _paddingScaleFactor(MediaQuery.of(context, MediaQueryAspect.textScaleFactor).textScaleFactor);
     final TextDirection? textDirection = Directionality.maybeOf(context);
 
     Widget? iconWidget;
@@ -962,7 +962,7 @@ class SimpleDialog extends StatelessWidget {
 
     // The paddingScaleFactor is used to adjust the padding of Dialog
     // children.
-    final double paddingScaleFactor = _paddingScaleFactor(MediaQuery.of(context).textScaleFactor);
+    final double paddingScaleFactor = _paddingScaleFactor(MediaQuery.of(context, MediaQueryAspect.textScaleFactor).textScaleFactor);
     final TextDirection? textDirection = Directionality.maybeOf(context);
 
     Widget? titleWidget;
