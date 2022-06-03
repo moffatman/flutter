68a3a81387fd955fa9fa11cefcb55a53300025e3
Callum Moffat <smartercallum@gmail.com>
2022-06-02 23:41:09 -0400

    Snappify gestures

--- packages/flutter/lib/src/gestures/arena.dart
+++ packages/flutter/lib/src/gestures/arena.dart
@@ -284,7 +284,7 @@ class GestureArenaManager {
           winner = bid.key;
         }
       }
-      if (winner != null && bestBid >= 1) {
+      if (winner != null && bestBid >= 0.5) {
         _resolveInFavorOf(pointer, state, winner);
       }
     }
