bbd6321c376e7cc4ab96767284e83aebcd9701f1
Callum Moffat <smartercallum@gmail.com>
2024-08-08 02:16:37 -0400

    Preserve TextAffinity when constructing fragment geometry

--- packages/flutter/lib/src/rendering/paragraph.dart
+++ packages/flutter/lib/src/rendering/paragraph.dart
@@ -1388,17 +1388,18 @@ class _SelectableFragment with Selectable, Diagnosticable, ChangeNotifier implem
       );
     }
 
-    final int selectionStart = _textSelectionStart!.offset;
-    final int selectionEnd = _textSelectionEnd!.offset;
-    final bool isReversed = selectionStart > selectionEnd;
-    final Offset startOffsetInParagraphCoordinates = paragraph._getOffsetForPosition(TextPosition(offset: selectionStart));
+    final TextPosition selectionStart = _textSelectionStart!;
+    final TextPosition selectionEnd = _textSelectionEnd!;
+    final bool isReversed = selectionStart.offset > selectionEnd.offset;
+    final Offset startOffsetInParagraphCoordinates = paragraph._getOffsetForPosition(selectionStart);
     final Offset endOffsetInParagraphCoordinates = selectionStart == selectionEnd
       ? startOffsetInParagraphCoordinates
-      : paragraph._getOffsetForPosition(TextPosition(offset: selectionEnd));
+      : paragraph._getOffsetForPosition(selectionEnd);
     final bool flipHandles = isReversed != (TextDirection.rtl == paragraph.textDirection);
     final TextSelection selection = TextSelection(
-      baseOffset: selectionStart,
-      extentOffset: selectionEnd,
+      baseOffset: selectionStart.offset,
+      extentOffset: selectionEnd.offset,
+      affinity: selectionStart.affinity,
     );
     final List<Rect> selectionRects = <Rect>[];
     for (final TextBox textBox in paragraph.getBoxesForSelection(selection)) {
