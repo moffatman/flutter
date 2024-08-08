9002a655be9f1fed2cc200e89315bd22bff31f61
Callum Moffat <smartercallum@gmail.com>
2024-08-08 02:16:37 -0400

    Preserve TextAffinity when constructing fragment geometry

--- packages/flutter/lib/src/rendering/paragraph.dart
+++ packages/flutter/lib/src/rendering/paragraph.dart
@@ -1567,18 +1567,22 @@ class _SelectableFragment
       return const SelectionGeometry(status: SelectionStatus.none, hasContent: true);
     }
 
-    final int selectionStart = _textSelectionStart!.offset;
-    final int selectionEnd = _textSelectionEnd!.offset;
-    final bool isReversed = selectionStart > selectionEnd;
+    final TextPosition selectionStart = _textSelectionStart!;
+    final TextPosition selectionEnd = _textSelectionEnd!;
+    final bool isReversed = selectionStart.offset > selectionEnd.offset;
     final Offset startOffsetInParagraphCoordinates = paragraph._getOffsetForPosition(
-      _textSelectionStart!,
+      selectionStart,
     );
     final Offset endOffsetInParagraphCoordinates = selectionStart == selectionEnd
         ? startOffsetInParagraphCoordinates
-        : paragraph._getOffsetForPosition(_textSelectionEnd!);
-    final flipHandles = isReversed != (TextDirection.rtl == paragraph.textDirection);
-    final selection = TextSelection(baseOffset: selectionStart, extentOffset: selectionEnd);
-    final selectionRects = <Rect>[];
+        : paragraph._getOffsetForPosition(selectionEnd);
+    final bool flipHandles = isReversed != (TextDirection.rtl == paragraph.textDirection);
+    final TextSelection selection = TextSelection(
+      baseOffset: selectionStart.offset,
+      extentOffset: selectionEnd.offset,
+      affinity: selectionStart.affinity,
+    );
+    final List<Rect> selectionRects = <Rect>[];
     for (final TextBox textBox in paragraph.getBoxesForSelection(selection)) {
       selectionRects.add(textBox.toRect());
     }
