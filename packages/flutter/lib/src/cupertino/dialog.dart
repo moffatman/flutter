2e80ef628c7f41c8774c6fd289ba4e83e22909de
Callum Moffat <smartercallum@gmail.com>
2025-05-03 15:55:17 -0400

    Fix double gesture processing in CupertinoAlertDialog
    If we already lost the pointer, it's not onEnd

--- packages/flutter/lib/src/cupertino/dialog.dart
+++ packages/flutter/lib/src/cupertino/dialog.dart
@@ -764,6 +764,7 @@ class _SlidingTapGestureRecognizer extends VerticalDragGestureRecognizer {
   ValueSetter<Offset>? onResponsiveEnd;
 
   int? _primaryPointer;
+  int? _lastRejectedPointer;
 
   @override
   void addAllowedPointer(PointerDownEvent event) {
@@ -773,6 +774,7 @@ class _SlidingTapGestureRecognizer extends VerticalDragGestureRecognizer {
 
   @override
   void rejectGesture(int pointer) {
+    _lastRejectedPointer = pointer;
     if (pointer == _primaryPointer) {
       _primaryPointer = null;
     }
@@ -803,7 +805,12 @@ class _SlidingTapGestureRecognizer extends VerticalDragGestureRecognizer {
       // scroll gestures when the latter give up.
       if (event is PointerUpEvent) {
         stopTrackingPointer(_primaryPointer!);
-        onResponsiveEnd?.call(event.position);
+        if (_lastRejectedPointer == event.pointer) {
+          onCancel?.call();
+        }
+        else {
+          onResponsiveEnd?.call(event.position);
+        }
         _primaryPointer = null;
         // Do not call `super.handleEvent`, which gives up the pointer and thus
         // rejects the gesture.
