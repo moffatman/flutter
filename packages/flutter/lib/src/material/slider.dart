9ef24b1b09cf1d9de9c52c2c22a9f7850452913d
Callum Moffat <smartercallum@gmail.com>
2025-07-28 19:37:27 -0400

    Add some debugOwners

--- packages/flutter/lib/src/material/slider.dart
+++ packages/flutter/lib/src/material/slider.dart
@@ -1187,14 +1187,14 @@ class _RenderSlider extends RenderBox with RelayoutWhenSystemFontsChangeMixin {
        _secondaryTrackValue = secondaryTrackValue {
     _updateLabelPainter();
     final team = GestureArenaTeam();
-    _drag = HorizontalDragGestureRecognizer()
+    _drag = HorizontalDragGestureRecognizer(debugOwner: this)
       ..team = team
       ..onStart = _handleDragStart
       ..onUpdate = _handleDragUpdate
       ..onEnd = _handleDragEnd
       ..onCancel = _endInteraction
       ..gestureSettings = gestureSettings;
-    _tap = TapGestureRecognizer()
+    _tap = TapGestureRecognizer(debugOwner: this)
       ..team = team
       ..onTapDown = _handleTapDown
       ..onTapUp = _handleTapUp
