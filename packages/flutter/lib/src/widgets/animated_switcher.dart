c12bc087d91a623e6f29d06c4b94b4be3ad92465
Callum Moffat <smartercallum@gmail.com>
2026-02-21 16:41:40 -0500

    Fix duplicate keys in AnimatedSwitcher
    KeyedSubtree.wrap does not always use the given key. it might use child's key
    
    we need to enforce unique keys (_childNumber)

--- packages/flutter/lib/src/widgets/animated_switcher.dart
+++ packages/flutter/lib/src/widgets/animated_switcher.dart
@@ -345,7 +345,10 @@ class _AnimatedSwitcherState extends State<AnimatedSwitcher> with TickerProvider
   }) {
     final entry = _ChildEntry(
       widgetChild: child,
-      transition: KeyedSubtree.wrap(builder(child, animation), _childNumber),
+      transition: KeyedSubtree(
+        key: ValueKey<int>(_childNumber),
+        child: builder(child, animation)
+      ),
       animation: animation,
       controller: controller,
     );
